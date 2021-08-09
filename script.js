54543fbe4b94411fefc15bb60c37236fc0990162
Mohammad Saad <81032267+masaad01@users.noreply.github.com>
2021-08-09 09:23:11 +0300

    added slider base

--- script.js
+++ script.js
@@ -112,11 +112,15 @@ class Scheduler{
       this.#myCourses.splice(index, 1);
   }
   _generateScheduleFunction(){
-    let arr = [];
-    for(let y=0;y<this.#myCourses.length;y++){
-      arr.push(this.#myCourses[y].sections[ random(0, this.#myCourses[y].sections.length - 1) ]); //choose random section from each course 
+    const schedules = [];
+    for(let i=0;i<10;i++){
+      const arr = [];
+      for(let y=0;y<this.#myCourses.length;y++){
+        arr.push(this.#myCourses[y].sections[ random(0, this.#myCourses[y].sections.length - 1) ]); //choose random section from each course 
+      }
+      schedules.push(arr);
     }
-    return arr;
+    return schedules;
   }
 }
 
@@ -207,15 +211,19 @@ class SchedulerGUI{
       }
     });
     self.#options["generateschedule"].submit.addEventListener("click",()=>{//when generate button clicked it will call the fucntion _generateSchedule
-      const schedule = self.#app._generateScheduleFunction();
       
-      let t = new TimeTable();
-      let twrap = document.getElementById("table");
+      const schedules = self.#app._generateScheduleFunction();
+      
+      let twrap = document.querySelector("#table .content");
       twrap.innerHTML = "";
-      twrap.appendChild(t.table);
-      for (const sec of schedule) {        
+      for (const schedule of schedules) {
+        let t = new TimeTable();
+        twrap.appendChild(t.table);
+        for (const sec of schedule) {        
         t.addSection(sec);
+        }
       }
+      
     });
 
     self.#myModal.submit.addEventListener("click", function(){
@@ -427,7 +435,7 @@ class TimeTable{
 
     const headTitles = ["#", "Sun", "Mon", "Tue", "Wed", "Thu"];
     for (const head of headTitles) {
-      htmlCreator("div", table, "", "tableHead",`<span>${head}</span>`);
+      htmlCreator("div", table, "", "tableHead",head);
     }
     const tableKeys = htmlCreator("div", table, "", "tableKeys");
     for(let i=8; i<=18; i++){
