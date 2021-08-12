ea5a516adb9281854c1a04dea8c1d3c08f3d9409
Mohammad Saad <81032267+masaad01@users.noreply.github.com>
2021-08-12 16:29:30 +0300

    section details update + search database update
    -added functionality to display section details in a modal
    -added semester property to Course class
    -added advancedSearch function to search database (or an array of courses) by multiple conditions

--- Scheduler.js
+++ Scheduler.js
@@ -1,4 +1,5 @@
 import searchDatabase from "./Database.js";
+import {advancedSearch}  from "./Database.js";
 
 const myCourses = [],
     schedule = [];
@@ -10,7 +11,7 @@ function courseIndex(courseNum) {
     });
 }
 function _searchFunction(val, searchBy) {
-  const result = searchDatabase(val, searchBy);
+  const result = advancedSearch("",false,["second","semester"],[val,searchBy,"and"]);
   return result;
 }
 function _addCourseFunction(courseNum) {
