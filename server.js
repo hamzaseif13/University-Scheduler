40cec8849f414823fe72c7111bb5702f1fd006d8
Mohammad Saad <81032267+masaad01@users.noreply.github.com>
2021-09-05 19:50:45 +0300

    added general search

--- server.js
+++ server.js
@@ -39,12 +39,32 @@ app.get("/generator", (req, res) => {
     
 });
 app.post("/getCourse/",async(req, res)=>{
-    const searchObj = {};
-    searchObj[req.body.searchBy] = {$regex:new RegExp(`^${req.body.value.trim()}.*`,"i")}
-    let search = await Course.find(searchObj).exec();
+    const validProps = ["semester", "faculty", "department", "lineNumber", "symbol", "name"];
+    let searchResult = [];
+    const searchBy = req.body.searchBy;
+    const value = req.body.value.trim();
+    const searchRegex = {$regex:new RegExp(`^${value}.*`,"i")};
+
+    if(validProps.includes(searchBy)){
+        const searchObj = {};
+        searchObj[searchBy] = searchRegex;
+        searchResult = await Course.find(searchObj).exec();
+    }
+    else if(searchBy === "all"){
+        const searchArr = [];
+        for (const prop of validProps) {
+            const obj = {};
+            obj[prop] = searchRegex;
+
+            searchArr.push(obj);
+        }
+        searchResult = await Course.find( { $or: searchArr } ).exec();
+    }
+    //{ $or: [ { status: "A" }, { qty: { $lt: 30 } } ] }
+    
     res.send({
-        courses: search,//.slice(0,10), 
-        num: search.length
+        courses: searchResult,//.slice(0,10), 
+        num: searchResult.length
     });
 })
 app.get("/login", (req, res) => {
